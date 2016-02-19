46062fb3d333da0b49b3e7cf9453c5926c5133da
Дворников Александр Николаевич <ip514s05@awp04.csc.local>
2016-02-19 14:56:25 +0600

    11

--- calc.c
+++ calc.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 int main(){
-	int a,b,c,x;
+	int a,b,c,x1,x2;
 	printf("vvedite koefficenty kvadratnogo uravneniia");
 	scanf("%d %d %d",&a,&b,&c);
-	int t=x*x*a+b*x+c;
+	int t=pow((b*b-4*a*c),0.5);
+      
 }
 
 
